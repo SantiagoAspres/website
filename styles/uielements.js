bfa522b2962ad894aaa8e607a503bed295e196f8
SantiagoAspres <santi.aspres@gmail.com>
2022-09-03 01:53:10 -0300

    modifico el index y agrego algunos estilos

--- styles/uielements.js
+++ styles/uielements.js
@@ -1,5 +1,21 @@
 import styled from "styled-components";
 
-export const MainContainer = styled.div`
-  background-color: #5d64ee;
+export const Hero = styled.header`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: papayawhip;
+`;
+
+export const HeroContent = styled.div`
+  max-width: 600px;
+  margin: 50px;
+`;
+
+export const Title = styled.h1`
+  font-size: 4em;
+  text-align: center;
+  color: palevioletred;
 `;

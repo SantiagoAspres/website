a3da65fd29eb0901368dfb30fe0839d3ff5e346b
SantiagoAspres <santi.aspres@gmail.com>
2024-03-22 20:38:24 -0300

    first render model

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,45 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import {Hero, HeroContent, Title} from "../styles/uielements";
+import {Canvas, useLoader} from '@react-three/fiber'
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
+import React, {Suspense} from 'react'
+import {OrbitControls} from '@react-three/drei'
+import styled from "styled-components";
+// import types from three
+const URL = '../model3d/cartoonSportCar/scene.gltf'
 
-export default function Home() {
-  return (
-      <div>
-          <Hero>
-              <HeroContent>
-                  <Title>
-                      Santiago Aspres
-                  </Title>
-              </HeroContent>
-          </Hero>
-      </div>
-  )
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh; /* Ajusta la altura al 100% del viewport */
+  background: antiquewhite;
+`
+
+function Model() {
+    const obj = useLoader(GLTFLoader, URL)
+
+    return <primitive object={obj.scene} position={[0, -1, 0]} scale={[2, 2, 2]}/>
+}
+
+export default function cartoonSportCar() {
+
+    return (
+
+        <Container>
+                    <Canvas
+                        camera={{
+                            position: [9, 7, 8.2],
+                            fov: 50,
+                            zoom: 1
+                        }}
+                    >
+                        <ambientLight color={'white'} intensity={0.3}/>
+                        <ambientLight color={'white'} intensity={1.7}/>
+                        <spotLight angle={0.15} color={'white'} penumbra={1} position={[10, 10, 10]}/>
+                        <Suspense>
+                            <Model/>
+                        </Suspense>
+                        <OrbitControls/>
+                    </Canvas>
+        </Container>
+    )
 }
